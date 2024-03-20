6a31565897c95b13a4eac997ffa6858599d404b2
TranTuanAnh <vt34023@gmail.com>
2024-03-20 16:34:50 +0700

    hi

---
+++ BulletObject.cpp
@@ -0,0 +1,35 @@
+﻿#include "BulletObject.h"
+
+BulletObject::BulletObject() {
+	bullet_time = 0;
+	is_move = false;
+}
+
+
+BulletObject :: ~BulletObject()
+{
+
+}
+
+
+// hàm này không cần nữa 
+
+/*void BulletObject::HandleMove(const int& x_border, const int& y_border)
+{
+	if (bullet_dir == DIR_RIGHT)
+	{
+		rect.x += x_val;
+		if (rect.x > x_border) {
+			is_move = false;
+		}
+	}
+	else if (bullet_dir == DIR_LEFT)
+	{
+		rect.x -= x_val;
+		if (rect.x < 0) {
+			is_move = false;
+		}
+	}
+}
+
+*/
---
+++ game_map.cpp
@@ -0,0 +1,51 @@
+#include"game_map.h"
+
+
+void GameMap::LoadMap(std::string path) {
+	std::ifstream in;
+	in.open(path.c_str());
+	for (int i = 0; i < MAX_MAP_Y; i++) {
+		for (int j = 0; j < MAX_MAP_X; j++) {
+			in >> game_map.tile[i][j];
+		}
+	}
+	in.close();
+}
+
+void GameMap::LoadTiles(SDL_Renderer* screen) {
+	char file_img[30];
+	FILE* fp = NULL;
+	for (int i = 0; i < MAX_TILE; i++) {
+		sprintf_s(file_img, "map1/%d.png", i);
+		fopen_s(&fp, file_img, "rb");
+		if (fp == NULL) {
+			continue;
+		}
+		fclose(fp);
+		tile_mat[i].LoadImg(file_img, screen);
+	}
+}
+
+void GameMap::DrawMap(SDL_Renderer* screen) {
+
+
+	int x1 = 185, x2 = 852, y1 = 15, y2 = 690;
+
+	int map_x = 0, map_y = 0;
+
+	for (int i = y1; i < y2; i += TILE_SIZE) {
+
+		map_x = 0;
+
+		for (int j = x1; j < x2; j += TILE_SIZE) {
+			int val = game_map.tile[map_y][map_x];
+
+			if (val >= 0) {
+				tile_mat[val].SetRect(j, i);
+				tile_mat[val].Render(screen);
+			}
+			map_x++;
+		}
+		map_y++;
+	}
+}
